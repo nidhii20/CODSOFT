0dd6531eb5aa2ab5e4bc84b27c1f7ca90a1b16a4
nidhii20 <117992476+nidhii20@users.noreply.github.com>
2023-08-24 12:58:21 +0530

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,1 +1,51 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    double a;
+    double b;
+
+    cout<<"Enter 1st number:";
+    cin>>a;
+
+    cout<<"Enter 2nd number:";
+    cin>>b;
+
+    char operation;
+    cout<<"Enter the operation to be performed:"
+    cin>>operation;
+
+    double result;
+
+    switch (operation)
+    {
+    case '+':
+    result=a+b;
+    break;
+
+    case '-':
+    result=a-b;
+    break;
+    
+case '*':
+result=a*b;
+break;
+
+case '/':
+if(b==0)
+cout<<"Division cannot be performed as denominator=0"<<endl;
+return 1;
+else{
+    result=a/b;
+    break;
+}
+
+    default:
+    cout<<"Enter valid operation!!"<<endl;
+    return 1;
+    break;
+    }
+cout<<"Result:"<<result<<endl;
+return 0;
+
+}
